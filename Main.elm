76bdc90ce707e3e762dfa87a8f81d911cc0f18ce
Andrey Kuzmin <unsoundscapes@gmail.com>
2016-02-28 14:14:00 +0100

    Animate and position

--- Main.elm
+++ Main.elm
@@ -1,14 +1,19 @@
 import Mouse
 import WebGL as GL
 import Math.Vector2 exposing (Vec2, vec2)
-import Math.Matrix4 as Mat4
 import Graphics.Element exposing (Element)
 import Task exposing (Task)
+import Time exposing (Time)
+
+
+currentFrame : Signal Int
+currentFrame =
+  Signal.foldp (\_ b -> (b + 1) % 24) 0 (Time.fps 15)
 
 
 main : Signal Element
 main =
-  Signal.map2 view Mouse.position texture.signal
+  Signal.map3 view Mouse.position currentFrame texture.signal
 
 
 type alias Vertex = { position : Vec2 }
@@ -39,55 +44,66 @@ mesh =
     ]
 
 
-ortho2D : Float -> Float -> Mat4.Mat4
-ortho2D w h =
-  Mat4.makeOrtho2D 0 w h 0
-
-
-view : (Int, Int) -> Maybe GL.Texture -> Element
-view (w, h) maybeTexture =
+view : (Int, Int) -> Int -> Maybe GL.Texture -> Element
+view (x, y) frame maybeTexture =
   GL.webgl
     (1024, 1024)
     ( case maybeTexture of
         Nothing ->
           []
         Just texture ->
-          render (1024, 1024) texture
+          render (1024, 1024) (x, y) frame texture
     )
 
 
-render : (Int, Int) -> GL.Texture -> List GL.Renderable
-render (w, h) texture =
-  [GL.render vertexShader fragmentShader mesh {resolution = vec2 (toFloat w) (toFloat h), sprite = texture}]
+render : (Int, Int) -> (Int, Int) -> Int -> GL.Texture -> List GL.Renderable
+render (w, h) (x, y) frame texture =
+  [ GL.render
+      vertexShader
+      fragmentShader
+      mesh
+      { resolution = vec2 (toFloat w) (toFloat h)
+      , offset = vec2 (toFloat x) (toFloat y)
+      , sprite = texture
+      , frame = frame
+      , size = vec2 (128 / toFloat (fst (GL.textureSize texture))) (256 / toFloat (snd (GL.textureSize texture)))
+      }
+  ]
 
 
 -- Shaders
 
-vertexShader : GL.Shader {attr | position : Vec2} {unif | resolution : Vec2} {texturePos : Vec2}
+vertexShader : GL.Shader {attr | position : Vec2} {unif | resolution : Vec2, offset : Vec2} {texturePos : Vec2}
 vertexShader = [glsl|
 
 attribute vec2 position;
+uniform vec2 offset;
 uniform vec2 resolution;
 varying vec2 texturePos;
 
 void main () {
-  vec2 clipSpace = position / resolution * 2.0 - 1.0;
+  vec2 clipSpace = (position + offset) / resolution * 2.0 - 1.0;
   gl_Position = vec4(clipSpace.x, -clipSpace.y, 0, 1);
-  texturePos = vec2(clipSpace.x, -clipSpace.y);
+  vec2 textureClipSpace = position / resolution * 2.0 - 1.0;
+  texturePos = vec2(textureClipSpace.x, -textureClipSpace.y);
 }
 
 |]
 
 
-fragmentShader : GL.Shader {} {u | sprite : GL.Texture } {texturePos : Vec2}
+fragmentShader : GL.Shader {} {u | sprite : GL.Texture, size : Vec2, frame : Int } {texturePos : Vec2}
 fragmentShader = [glsl|
 
 precision mediump float;
 uniform sampler2D sprite;
+uniform vec2 size;
+uniform int frame;
 varying vec2 texturePos;
 
 void main () {
-  gl_FragColor = texture2D(sprite, texturePos);
+  int frames = int(1.0 / size.x);
+  vec2 frameOffset = size * vec2(float(frame - frame / frames * frames), -float(frame / frames));
+  gl_FragColor = texture2D(sprite, texturePos + frameOffset);
 }
 
 |]
