d9c68f9d249f226e3f606b066a4056bf6611c8d9
Andrey Kuzmin <andrey.kuzmin@soundcloud.com>
2016-12-02 21:26:57 +0100

    Subscribe to resizes

--- Tangram.elm
+++ Tangram.elm
@@ -1,14 +1,28 @@
 module Tangram exposing (main)
 
-import Tangram.Shader as Shader exposing (Uniform, Varying, Attribute)
-import Math.Vector3 exposing (Vec3, vec3)
-import Math.Matrix4 exposing (makeRotate, makeScale, makeTranslate, mul, Mat4)
-import Html exposing (Html)
-import Html.Attributes exposing (width, height)
-import WebGL
 import AnimationFrame
+import Html exposing (Html)
+import Html.Attributes exposing (width, height, style)
+import Math.Matrix4 exposing (makeRotate, makeScale, makeTranslate, mul, Mat4)
+import Math.Vector3 exposing (Vec3, vec3)
 import Tangram.Mesh as Mesh
+import Tangram.Shader as Shader exposing (Uniform, Varying, Attribute)
 import Tangram.Shape as Shape exposing (Shape, Position)
+import WebGL
+import Window
+import Time exposing (Time)
+import Task
+
+
+type Message
+    = Resize Window.Size
+    | Tick Time
+
+
+type alias Model =
+    { size : Window.Size
+    , time : Time
+    }
 
 
 shapes : List Shape
@@ -22,40 +36,65 @@ shapes =
         |> (::) Shape.elm
 
 
-main : Program Never Float Float
+main : Program Never Model Message
 main =
     Html.program
-        { init = ( 0, Cmd.none )
-        , view = \t -> view (t * 0.001)
-        , update = \dt t -> ( t + dt, Cmd.none )
-        , subscriptions = always (AnimationFrame.diffs identity)
+        { init = ( Model (Window.Size 0 0) 0, Task.perform Resize Window.size )
+        , view = \{ time, size } -> view size (time * 0.001)
+        , update = update
+        , subscriptions = subscriptions
         }
 
 
-view : Float -> Html Float
-view t =
+subscriptions : Model -> Sub Message
+subscriptions model =
+    Sub.batch
+        [ AnimationFrame.diffs Tick
+        , Window.resizes Resize
+        ]
+
+
+update : Message -> Model -> ( Model, Cmd Message )
+update message model =
+    case message of
+        Tick time ->
+            { model | time = model.time + time } ! []
+
+        Resize size ->
+            { model | size = size } ! []
+
+
+view : Window.Size -> Time -> Html Message
+view size t =
     let
         shape =
             interpolate shapes t
+
+        ratio =
+            toFloat size.width / toFloat size.height
     in
-        WebGL.toHtml [ width 400, height 400 ]
-            [ render Mesh.parallelepiped colors.green (scaleMat 1) shape.parallelepiped t
-            , render Mesh.tetrahedron colors.orange (scaleMat 2) shape.orangeTetrahedron1 t
-            , render Mesh.cube colors.green (scaleMat 2) shape.cube t
-            , render Mesh.tetrahedron colors.orange (scaleMat 2) shape.orangeTetrahedron2 t
-            , render Mesh.tetrahedron colors.gray (scaleMat 4) shape.grayTetrahedron t
-            , render Mesh.tetrahedron colors.blue (scaleMat 4) shape.blueTetrahedron2 t
-            , render Mesh.tetrahedron colors.blue (scaleMat (sqrt 2 * 2)) shape.blueTetrahedron1 t
+        WebGL.toHtml
+            [ width (size.width * 2)
+            , height (size.height * 2)
+            , style [ ( "transform", "scale(0.5)" ), ( "transform-origin", "0 0" ) ]
+            ]
+            [ render ratio Mesh.parallelepiped colors.green (scaleMat 1) shape.parallelepiped t
+            , render ratio Mesh.tetrahedron colors.orange (scaleMat 2) shape.orangeTetrahedron1 t
+            , render ratio Mesh.cube colors.green (scaleMat 2) shape.cube t
+            , render ratio Mesh.tetrahedron colors.orange (scaleMat 2) shape.orangeTetrahedron2 t
+            , render ratio Mesh.tetrahedron colors.gray (scaleMat 4) shape.grayTetrahedron t
+            , render ratio Mesh.tetrahedron colors.blue (scaleMat 4) shape.blueTetrahedron2 t
+            , render ratio Mesh.tetrahedron colors.blue (scaleMat (sqrt 2 * 2)) shape.blueTetrahedron1 t
             ]
 
 
-render : WebGL.Drawable Attribute -> Vec3 -> Mat4 -> Position -> Float -> WebGL.Renderable
-render shape color scaleMat { rotateZ, rotateY, x, y, z } t =
+render : Float -> WebGL.Drawable Attribute -> Vec3 -> Mat4 -> Position -> Float -> WebGL.Renderable
+render ratio shape color scaleMat { rotateZ, rotateY, x, y, z } t =
     WebGL.render
         Shader.vertex
         Shader.fragment
         shape
-        (Uniform color (rotateMat rotateZ rotateY t) scaleMat (translateMat x y z t) camera)
+        (Uniform color (rotateMat rotateZ rotateY t) scaleMat (translateMat x y z t) (camera ratio))
 
 
 colors :
@@ -72,8 +111,8 @@ colors =
     }
 
 
-camera : Mat4
-camera =
+camera : Float -> Mat4
+camera ratio =
     let
         eye =
             vec3 0 0 30
@@ -81,7 +120,7 @@ camera =
         center =
             vec3 0 0 0
     in
-        mul (Math.Matrix4.makePerspective 45 1 0.01 100)
+        mul (Math.Matrix4.makePerspective 45 ratio 0.01 100)
             (Math.Matrix4.makeLookAt eye center Math.Vector3.j)
 
 
